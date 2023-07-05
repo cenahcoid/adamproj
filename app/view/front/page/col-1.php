66697a56e7ce2471d6d4aec3f35ed8e6196bc057
Shironeko172 <thoyaht@gmail.com>
2023-07-05 20:03:35 +0700

    Fix.sidebar

--- app/view/front/page/col-1.php
+++ app/view/front/page/col-1.php
@@ -1,10 +1,8 @@
 <!DOCTYPE html>
 <html>
   <?php $this->getThemeElement('page/html/head', $__forward) ?>
-  <body>
+  <body class="sb-nav-fixed">
     <?php $this->getThemeElement('page/html/navbar', $__forward)?>
-    <?php $this->getThemeElement('page/html/sidebar', $__forward)?>
-    <?php $this->getThemeElement('page/html/footer', $__forward)?>
     <?php $this->getThemeContent() ?>
 
     <!-- jQuery, Bootstrap.js, jQuery plugins and Custom JS code -->
--- app/view/front/page/html/navbar.php
+++ app/view/front/page/html/navbar.php
@@ -1,18 +1,25 @@
-<div class="navbar bg-primary text-white">
-    <div class="container-fluid">
-        <a href="<?= base_url() ?>" class="mx-3 text-white text-decoration-none">
-            <h1 class="h2">Elysia Book</h1>
-        </a>
-        <div class="dropdown">
-            <a href="#" class="mx-5 text-white text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
-                <img src="assets/img/icon.gif" alt="hugenerd" width="30" height="30" class="rounded-circle">
-                <span class="d-none d-sm-inline mx-1"><?= $sess->user->nama ?></span>
-            </a>
-            <ul class="dropdown-menu dropdown-menu-light text-small shadow">
-                <li><a class="dropdown-item" href="<?php if (isset($sess->user->id)) { ?>
-                        <?= base_url('logout');
-                                                    } ?>">Sign out</a></li>
-            </ul>
+<nav class="sb-topnav navbar navbar-expand navbar-primary bg-primary">
+    <!-- Navbar Brand-->
+    <a class="navbar-brand ps-3" href="<?php base_url() ?>"><h1 class="h2 text-white">Elysia Book</h1></a>
+    <!-- Sidebar Toggle-->
+    <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="bi bi-arrow-bar-left"></i></button>
+    <!-- Navbar Search-->
+    <form class="d-none d-md-inline-block form-inline ms-auto me-0 me-md-3 my-2 my-md-0">
+        <div class="input-group">
+            <input hidden class="form-control" type="text" placeholder="Search for..." aria-label="Search for..." aria-describedby="btnNavbarSearch" />
+            <button hidden class="btn btn-dark" id="btnNavbarSearch" type="button"><i class="bi bi-search"></i></button>
         </div>
-    </div>
-</div>
+    </form>
+    <!-- Navbar-->
+    <ul class="navbar-nav ms-auto ms-md-0 me-3 me-lg-4">
+        <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                <i class="bi bi-person-circle text-white"></i>
+                <span class="d-none d-sm-inline mx-1 text-white"><?= $sess->user->nama ?></span></a>
+            <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                <li><a class="dropdown-item" href="<?php if (isset($sess->user->id)) { ?><?= base_url('logout');
+                                                                                        } ?>">Logout</a></li>
+            </ul>
+        </li>
+    </ul>
+</nav>
