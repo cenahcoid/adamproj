846438ccc78c8940a3188c3faeb857ad0f3c38cb
Cenah <thoyaht@gmail.com>
2023-07-06 15:44:27 +0700

    Feat.New_Table

--- sql/adamproj.sql
+++ sql/adamproj.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 04 Jul 2023 pada 06.13
--- Versi server: 10.4.28-MariaDB
--- Versi PHP: 8.2.4
+-- Generation Time: Jul 06, 2023 at 10:41 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,7 +24,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `a_apikey`
+-- Table structure for table `a_apikey`
 --
 
 CREATE TABLE `a_apikey` (
@@ -37,17 +37,26 @@ CREATE TABLE `a_apikey` (
   `is_active` int(1) NOT NULL DEFAULT 1 COMMENT '1 active, 0 inactive'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='API Key storage';
 
+-- --------------------------------------------------------
+
 --
--- Dumping data untuk tabel `a_apikey`
+-- Table structure for table `b_karyawan`
 --
 
-INSERT INTO `a_apikey` (`nation_code`, `id`, `code`, `name`, `cdate`, `ldate`, `is_active`) VALUES
-(62, 1, 'ABCD1234', 'General APIKEY', '2020-06-09 09:47:18', '2020-06-09 02:47:18', 1);
+CREATE TABLE `b_karyawan` (
+  `id` int(6) NOT NULL,
+  `nama` varchar(255) DEFAULT NULL,
+  `jabatan` varchar(255) DEFAULT NULL,
+  `no_hp` int(15) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `alamat` text DEFAULT NULL,
+  `ditugaskan` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `b_member`
+-- Table structure for table `b_member`
 --
 
 CREATE TABLE `b_member` (
@@ -57,10 +66,22 @@ CREATE TABLE `b_member` (
   `alamat` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `b_member`
+--
+
+INSERT INTO `b_member` (`id`, `nama`, `email`, `alamat`) VALUES
+(8, 'Adam', 'shizualpha@gmail.com', 'Kp.Legok Nangka'),
+(9, 'Elysia', 'elysiachan@gmail.com', 'Armada'),
+(10, 'Syahdan', 'syahdan@gmail.com', 'Kp.Pasir Pendeuy'),
+(11, 'Rosa', 'Rosa@gmail.com', 'Cikancung'),
+(12, 'Hadi', 'Hadi@gmail.com', 'Panyirapan'),
+(13, 'Kiana Kaslana', 'Kiana@gmail.com', 'Armada');
+
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `b_user`
+-- Table structure for table `b_user`
 --
 
 CREATE TABLE `b_user` (
@@ -68,22 +89,21 @@ CREATE TABLE `b_user` (
   `nama` varchar(255) DEFAULT NULL,
   `email` varchar(255) DEFAULT NULL,
   `password` varchar(255) DEFAULT NULL,
-  `token` varchar(255) DEFAULT NULL,
   `is_active` enum('1','2','','') DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data untuk tabel `b_user`
+-- Dumping data for table `b_user`
 --
 
-INSERT INTO `b_user` (`id`, `nama`, `email`, `password`, `token`, `is_active`) VALUES
-(1, 'adam', 'shizu@gmail.com', '$2y$10$NkiI9Bzfhu.qZn5pCaybwOeqZ5/wK5TCSvrz99.N3mp1Cs4Uz9nHW', '123456', '1'),
-(2, 'adam', 'kiryu@gmail.com', '$2y$10$LJlrHHhYpSoCRNJtw2IixuV2a36jdvvaHkJVOMNQ9sWaR99XpvbFK', NULL, '1');
+INSERT INTO `b_user` (`id`, `nama`, `email`, `password`, `is_active`) VALUES
+(1, 'adam', 'shizu@gmail.com', '$2y$10$NkiI9Bzfhu.qZn5pCaybwOeqZ5/wK5TCSvrz99.N3mp1Cs4Uz9nHW', '1'),
+(2, 'adam', 'kiryu@gmail.com', '$2y$10$LJlrHHhYpSoCRNJtw2IixuV2a36jdvvaHkJVOMNQ9sWaR99XpvbFK', '1');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `c_jenis_alat`
+-- Table structure for table `c_jenis_alat`
 --
 
 CREATE TABLE `c_jenis_alat` (
@@ -92,7 +112,7 @@ CREATE TABLE `c_jenis_alat` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data untuk tabel `c_jenis_alat`
+-- Dumping data for table `c_jenis_alat`
 --
 
 INSERT INTO `c_jenis_alat` (`id`, `jenis`) VALUES
@@ -105,7 +125,7 @@ INSERT INTO `c_jenis_alat` (`id`, `jenis`) VALUES
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `c_jenis_buku`
+-- Table structure for table `c_jenis_buku`
 --
 
 CREATE TABLE `c_jenis_buku` (
@@ -114,7 +134,7 @@ CREATE TABLE `c_jenis_buku` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data untuk tabel `c_jenis_buku`
+-- Dumping data for table `c_jenis_buku`
 --
 
 INSERT INTO `c_jenis_buku` (`id`, `jenis`) VALUES
@@ -130,34 +150,7 @@ INSERT INTO `c_jenis_buku` (`id`, `jenis`) VALUES
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `c_jenis_teknologi`
---
-
-CREATE TABLE `c_jenis_teknologi` (
-  `id` int(6) NOT NULL,
-  `jenis` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data untuk tabel `c_jenis_teknologi`
---
-
-INSERT INTO `c_jenis_teknologi` (`id`, `jenis`) VALUES
-(2, 'accessories'),
-(9, 'drawing device'),
-(6, 'headset'),
-(1, 'kabel dan konektor'),
-(5, 'keyboard'),
-(3, 'monitor'),
-(4, 'mouse'),
-(10, 'mousepad'),
-(7, 'speaker'),
-(8, 'webcam & modem');
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `d_alat`
+-- Table structure for table `d_alat`
 --
 
 CREATE TABLE `d_alat` (
@@ -172,7 +165,7 @@ CREATE TABLE `d_alat` (
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `d_buku`
+-- Table structure for table `d_buku`
 --
 
 CREATE TABLE `d_buku` (
@@ -187,152 +180,117 @@ CREATE TABLE `d_buku` (
   `harga` int(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `d_teknologi`
---
-
-CREATE TABLE `d_teknologi` (
-  `id` int(6) NOT NULL,
-  `kode_barang` varchar(255) NOT NULL,
-  `nm_barang` varchar(255) DEFAULT NULL,
-  `jenis` varchar(255) DEFAULT NULL,
-  `jumlah` int(255) DEFAULT NULL,
-  `harga` int(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indeks untuk tabel `b_member`
+-- Indexes for table `b_karyawan`
+--
+ALTER TABLE `b_karyawan`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `b_member`
 --
 ALTER TABLE `b_member`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indeks untuk tabel `b_user`
+-- Indexes for table `b_user`
 --
 ALTER TABLE `b_user`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indeks untuk tabel `c_jenis_alat`
+-- Indexes for table `c_jenis_alat`
 --
 ALTER TABLE `c_jenis_alat`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `jenis` (`jenis`);
 
 --
--- Indeks untuk tabel `c_jenis_buku`
+-- Indexes for table `c_jenis_buku`
 --
 ALTER TABLE `c_jenis_buku`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `jenis` (`jenis`);
 
 --
--- Indeks untuk tabel `c_jenis_teknologi`
---
-ALTER TABLE `c_jenis_teknologi`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `jenis` (`jenis`);
-
---
--- Indeks untuk tabel `d_alat`
+-- Indexes for table `d_alat`
 --
 ALTER TABLE `d_alat`
   ADD PRIMARY KEY (`id`),
   ADD KEY `jenis` (`jenis`);
 
 --
--- Indeks untuk tabel `d_buku`
+-- Indexes for table `d_buku`
 --
 ALTER TABLE `d_buku`
   ADD PRIMARY KEY (`id`),
   ADD KEY `jenis` (`jenis`);
 
 --
--- Indeks untuk tabel `d_teknologi`
---
-ALTER TABLE `d_teknologi`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `jenis` (`jenis`);
-
---
--- AUTO_INCREMENT untuk tabel yang dibuang
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT untuk tabel `b_member`
+-- AUTO_INCREMENT for table `b_karyawan`
 --
-ALTER TABLE `b_member`
+ALTER TABLE `b_karyawan`
   MODIFY `id` int(6) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT untuk tabel `b_user`
+-- AUTO_INCREMENT for table `b_member`
+--
+ALTER TABLE `b_member`
+  MODIFY `id` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT for table `b_user`
 --
 ALTER TABLE `b_user`
   MODIFY `id` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT untuk tabel `c_jenis_alat`
+-- AUTO_INCREMENT for table `c_jenis_alat`
 --
 ALTER TABLE `c_jenis_alat`
   MODIFY `id` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
--- AUTO_INCREMENT untuk tabel `c_jenis_buku`
+-- AUTO_INCREMENT for table `c_jenis_buku`
 --
 ALTER TABLE `c_jenis_buku`
   MODIFY `id` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
--- AUTO_INCREMENT untuk tabel `c_jenis_teknologi`
---
-ALTER TABLE `c_jenis_teknologi`
-  MODIFY `id` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
-
---
--- AUTO_INCREMENT untuk tabel `d_alat`
+-- AUTO_INCREMENT for table `d_alat`
 --
 ALTER TABLE `d_alat`
   MODIFY `id` int(6) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT untuk tabel `d_buku`
+-- AUTO_INCREMENT for table `d_buku`
 --
 ALTER TABLE `d_buku`
   MODIFY `id` int(6) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT untuk tabel `d_teknologi`
---
-ALTER TABLE `d_teknologi`
-  MODIFY `id` int(6) NOT NULL AUTO_INCREMENT;
-
---
--- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+-- Constraints for dumped tables
 --
 
 --
--- Ketidakleluasaan untuk tabel `d_alat`
+-- Constraints for table `d_alat`
 --
 ALTER TABLE `d_alat`
   ADD CONSTRAINT `d_alat_ibfk_1` FOREIGN KEY (`jenis`) REFERENCES `c_jenis_alat` (`jenis`);
 
 --
--- Ketidakleluasaan untuk tabel `d_buku`
+-- Constraints for table `d_buku`
 --
 ALTER TABLE `d_buku`
   ADD CONSTRAINT `d_buku_ibfk_1` FOREIGN KEY (`jenis`) REFERENCES `c_jenis_buku` (`jenis`);
-
---
--- Ketidakleluasaan untuk tabel `d_teknologi`
---
-ALTER TABLE `d_teknologi`
-  ADD CONSTRAINT `d_teknologi_ibfk_1` FOREIGN KEY (`jenis`) REFERENCES `c_jenis_teknologi` (`jenis`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
